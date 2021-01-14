7a4c9ca6cc8ced4b2f10b0769cb54d9d7b13909d
hpradhan <35611773+hpradhan@users.noreply.github.com>
2021-01-14 17:26:45 -0500

    Update join_training_code.sql

--- join_training_code.sql
+++ join_training_code.sql
@@ -14,8 +14,21 @@ from volunteers
 left join Organizers 
 on volunteers.turf_id=Organizers.turf_id;
 
--- left right example 
+-- right join example 
 -- Goal: To see that right and left joins work similarly but are based on order
 select *
 from Organizers 
 left join volunteers using(turf_id);
+
+-- left join example
+-- goal: to see which organizer is working with which regional
+select *
+from organizers
+left join Regionals
+on organizers.region_id=Regionals.region_id
+
+-- switched table order
+select *
+from Regionals
+left join organizers 
+on organizers.region_id=Regionals.region_id
