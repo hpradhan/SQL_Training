d3101506eefeb7d0a31bc631ea863a9448372f4e
hpradhan <35611773+hpradhan@users.noreply.github.com>
2021-01-15 13:18:52 -0500

    Update join_training_code.sql

--- join_training_code.sql
+++ join_training_code.sql
@@ -12,7 +12,7 @@ select *
 from Shifts;
 
 -- prompts
---- 1. Analyze which volunteers have shift information
---- 2. Which volunteers live in what organizer's turf
---- 3. Which organizer is working with which regional?
+--- 1. Which volunteers have shift information?
+--- 2. Which volunteers live in an organizer's turf?
+--- 3. Which organizer are working with which regional?
 --- 3a. Same question with a right join. 
